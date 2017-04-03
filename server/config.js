1aa11152836159c33b67fd04ea60c171a4f73e02
Calvin Ferrando <karuban@Calvins-MacBook-Pro.local>
2017-04-04 01:53:08 +0800

    Add passport.js and setup basic auth methods

--- server/config.js
+++ server/config.js
@@ -3,3 +3,8 @@ export const db = {
   port: 28015,
   db: 'expertsdb'
 };
+
+export const auth = {
+  passwordSalt: 'yJCnH1OEJ7YIo2R1nkcqE1GANVykS0Mb6dZeijHjfGBQvYsRcPkVk0NGkGx4yDpr',
+  sessionSecret: 'qXX5lsVUAR6wFIxpNKPFBBE2A7kRAlcFDIYIBwPu5nskCIdKerfY0f7FHs8OJF6t'
+};

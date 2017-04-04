a9855622d6549f2de09b3d37d8198c96cc170d7f
Calvin Ferrando <karuban@Calvins-MacBook-Pro.local>
2017-04-04 14:57:08 +0800

    Send JWT back after login, and add tests for it

--- server/config.js
+++ server/config.js
@@ -6,5 +6,6 @@ export const db = {
 
 export const auth = {
   passwordSalt: 'yJCnH1OEJ7YIo2R1nkcqE1GANVykS0Mb6dZeijHjfGBQvYsRcPkVk0NGkGx4yDpr',
-  sessionSecret: 'qXX5lsVUAR6wFIxpNKPFBBE2A7kRAlcFDIYIBwPu5nskCIdKerfY0f7FHs8OJF6t'
+  sessionSecret: 'qXX5lsVUAR6wFIxpNKPFBBE2A7kRAlcFDIYIBwPu5nskCIdKerfY0f7FHs8OJF6t',
+  jwtSecret: 'hmwjMWyDsF31ru9N7ebJu1aWar5KzSuv7ZuyRRwVRI0SYwTa4CAGEs09lIf9FoG0',
 };

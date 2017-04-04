82922afb45205350f66ddf41033fcf20bbcecbb2
Calvin Ferrando <karuban@Calvins-MacBook-Pro.local>
2017-04-04 16:13:18 +0800

    Do not return password during auth and in profiles

--- server/src/user/get.js
+++ server/src/user/get.js
@@ -11,8 +11,11 @@ export default (app) => {
       res.send(req.user);
       return;
     }
+
     try {
-      const user = await User.get(req.params.id);
+      const user = await User.get(req.params.id)
+        .without(['password'])
+        .execute();
       res.send(user);
     } catch (e) {
       res.status(400).send({error: 'User does not exist'});
